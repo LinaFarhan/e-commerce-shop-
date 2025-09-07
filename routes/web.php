fc8c8a61f067d1cf56cebf14bd211b6c01d68415
lina <linasaif333@gmail.com>
2025-09-07 04:38:01 +0300

    feat:create storecontroller and routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\StoreController;
- 
+
 
  Route::get('/shop',[ShopController::class,'index']);
 
@@ -14,7 +14,7 @@ Route::get('/products/{id}', [StoreController::class, 'productDetails'])->name('
 
 Route::get('/cart', [StoreController::class, 'cart'])->name('shop.cart');
 
-Route::get('/about-us', [StoreController::class, 'about'])->name('shop.about');
+Route::get('/about-us', [StoreController::class, 'about'])->name('shop.about-us');
 
 Route::get('/contact', [StoreController::class, 'contact'])->name('shop.contact');
 Route::post('/contact', [StoreController::class, 'submitContact'])->name('shop.contact.submit');
